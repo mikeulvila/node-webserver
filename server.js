0ea62537ed78bad1a650da6485e4f0c5715ddf9e
Mike Ulvila <mikeulvila@me.com>
2016-02-02 13:35:47 -0600

    using route params to execute node-cal in browser

--- server.js
+++ server.js
@@ -1,36 +1,48 @@
 'use strict'
-
-const http = require('http');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  if (req.url === '/hello') {
-    const msg = '<h1>Hello World!</h1>';
+app.get('/hello', (req, res) => {
+  const name = req.query.name;
+
+  const msg = `<h1>Hello ${name}!</h1>`;
 
     res.writeHead(200, {
-      'Content-type': 'tex/html'
+      'Content-type': 'text/html'
     });
-
+    // chunk response by character
     msg.split('').forEach((char, i) => {
       setTimeout(() => {
         res.write(char);
       }, 1000*i);
     });
+    // wait for all characters to be sent
     setTimeout(() => {
       res.end();
     }, 20000);
-  } else if (req.url === '/random') {
-    res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    res.end('Access Denied');
-  }
-  res.writeHead(200, {
-    "Content-type": "text/html"
-  });
+});
+
+app.get('/cal/:month/:year', (req, res) => {
+  const monthJS = require('node-cal/lib/month');
+  var month = parseInt(req.params.month);
+  var year = parseInt(req.params.year);
+  res.end(`${monthJS.monthToString(month, year)}`);
+});
+
+app.get('/random/:min/:max', (req, res) => {
+  var min = parseInt(req.params.min);
+  var max = parseInt(req.params.max);
+  console.log(min);
+  console.log(max);
+  res.end((Math.floor(Math.random() * (max - min + 1)) + min).toString());
+});
+
+app.all('*', (req, res) => {
+  res.status(403).send('Access Denied!');
+});
 
 
-}).listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`node.js server started. listening on port ${PORT}`);
 });
