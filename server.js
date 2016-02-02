3b552a1ec6db1018c9609abef3d91cd07d5b1c29
Mike Ulvila <mikeulvila@me.com>
2016-02-02 10:52:32 -0600

    added package.json

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 'use strict'
 
 const http = require('http');
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   console.log(req.method, req.url);
+  if (req.url === '/hello') {
+    const msg = '<h1>Hello World!</h1>';
 
-  debugger;
+    res.writeHead(200, {
+      'Content-type': 'tex/html'
+    });
 
+    msg.split('').forEach((char, i) => {
+      setTimeout(() => {
+        res.write(char);
+      }, 1000*i);
+    });
+    setTimeout(() => {
+      res.end();
+    }, 20000);
+  } else if (req.url === '/random') {
+    res.end(Math.random().toString());
+  } else {
+    res.writeHead(403);
+    res.end('Access Denied');
+  }
   res.writeHead(200, {
     "Content-type": "text/html"
   });
 
-
-  res.end('<h1>Hello!!!</h1>');
 
 }).listen(PORT, () => {
   console.log(`node.js server started. listening on port ${PORT}`);
