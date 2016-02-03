2c44861b859efdc6d36e048dc3f8989e2c3624a5
Mike Ulvila <mikeulvila@me.com>
2016-02-03 14:45:21 -0600

    added jquery and sass

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 'use strict'
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
 
 app.get('/', (req, res) => {
   res.render('index', {
