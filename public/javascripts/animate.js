5f87d3fa900444d42736704891991a1d096fd83f
Mike Ulvila <mikeulvila@me.com>
2016-02-04 08:18:48 -0600

    playing with sass and jquery

---
+++ public/javascripts/animate.js
@@ -0,0 +1,8 @@
+(function ($) {
+
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+
+
+}(jQuery));
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Super Cool App',
+    title: 'The Worst Calendar Ever',
     date: new Date()
   });
 });
