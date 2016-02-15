a1c818dba482890b8a081e1c12753175d18d92f2
Mike Ulvila <mikeulvila@me.com>
2016-02-15 12:54:06 -0600

    switched / path back to original

--- routes/home.js
+++ routes/home.js
@@ -9,13 +9,11 @@ const News = require('../models/News');
 // get for index
 router.get('/', (req, res) => {
   News.findOne().sort('-_id').exec((err, doc) => {
+
     if (err) throw err;
-
-    const topStory = doc && doc.top && doc.top[0] || '';
-
     res.render('index', {
       date: new Date(),
-      topStory: topStory
+      topHeadline: doc.top[0]
     });
   });
 });
