c6e57b9a8e42b8fafc54123ea45924344f936c2e
Mike Ulvila <mikeulvila@me.com>
2016-02-15 11:54:48 -0600

    added guard statement to home.js

--- routes/home.js
+++ routes/home.js
@@ -9,11 +9,13 @@ const News = require('../models/News');
 // get for index
 router.get('/', (req, res) => {
   News.findOne().sort('-_id').exec((err, doc) => {
-
     if (err) throw err;
+
+    const topStory = doc && doc.top && doc.top[0] || '';
+
     res.render('index', {
       date: new Date(),
-      topHeadline: doc.top[0]
+      topStory: topStory
     });
   });
 });
